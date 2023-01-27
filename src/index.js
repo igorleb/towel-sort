fb9f711960397caaa5d484094a69c1f4796e42c1
igorleb <ip.lebedev07@gmail.com>
2023-01-27 22:34:43 +0400

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let arr = [];
+    if (matrix)
+        for (let i = 0; i < matrix.length; i++) {
+            getValArr(matrix[i], arr, i % 2 == 1);
+        }
+    return arr;
+};
